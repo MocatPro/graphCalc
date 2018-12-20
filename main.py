d62df56a982b1dfc865007a14dfcd56461b3ee91
Sean Funk <30637155+MocatPro@users.noreply.github.com>
2018-12-20 17:39:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,12 @@
-import math
+import math #I could probably delete this. If I were to add to the program, this could come in handy.
 segmentArea = []
 points = [0]
 totalArea = 0
 c = "nope"
-
 x = 0
-
-
-
-
-
 def get_segment(a, b):
     sa = ((a + b) / 2) * 10
     return sa
-
 
 while c != 'done':
     c = (input(": "))
@@ -29,9 +22,7 @@ for coord in points:
     print(x)
     segmentArea.append(get_segment(points[x-1], points[x]))
     
-
 for item in segmentArea:
     totalArea = totalArea + int(item)
-
 print(totalArea)
 
